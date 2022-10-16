65b7ed41863638151ac0ff7c8e8f0358b2b20ad5
aliasooo <onealmarange@gmail.com>
2022-10-16 13:50:46 +0200

    Feature/authentication (#2)
    * install clarity package
    
    * create application modules
    
    Co-authored-by: onealmrnge <oneal@quatrohaus.com>

---
+++ src/app/modules/satellites/satellites.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SatellitesRoutingModule } from './satellites-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SatellitesRoutingModule
+  ]
+})
+export class SatellitesModule { }
---
+++ src/app/modules/spy-stuff/spy-stuff.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpyStuffRoutingModule } from './spy-stuff-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SpyStuffRoutingModule
+  ]
+})
+export class SpyStuffModule { }
